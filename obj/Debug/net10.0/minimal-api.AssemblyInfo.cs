bf1f8d68c059c4fb85a1b952e4e963e744e43d3b
Jhonny Araújo <jhonny.a.santana2002@gmail.com>
2025-11-01 13:43:19 -0300

    chore(data): adiciona Entity Framework Core Tools

--- obj/Debug/net10.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931526f1ad2b1ad538504893ad7ecd000dbd1411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d1092e64861977c30b5e706c174708e85eac5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
