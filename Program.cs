befd8c05189f231773c56d1fe896af5354bbc11c
Jhonny Araújo <jhonny.a.santana2002@gmail.com>
2025-11-01 14:06:31 -0300

    refactor(domain): move LoginDTO do Program.cs para diretório DTOs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
 {
   if (loginDTO.Email == "adm@email.com" && loginDTO.Senha == "123456")
   {
@@ -16,9 +16,3 @@ app.MapPost("/login", (LoginDTO loginDTO) =>
 });
 
 app.Run();
-
-public class LoginDTO
-{
-  public string Email { get; set; } = default!;
-  public string Senha { get; set; } = default!;
-}
