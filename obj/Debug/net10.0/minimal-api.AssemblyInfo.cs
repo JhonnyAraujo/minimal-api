ba4d926c9157375922ed58a718138f2d493529e2
Jhonny Araújo <jhonny.a.santana2002@gmail.com>
2025-11-01 13:13:56 -0300

    feat(auth): adiciona endpoint de login e classe LoginDTO

--- obj/Debug/net10.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55884ec81318c683eaedb6bfc5274af4475ef269")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
